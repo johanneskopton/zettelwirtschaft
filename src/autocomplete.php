7d01c336c3bf6cee94050c62d010410a34949264
Johannes Kopton <johanneskopton@gmail.com>
2021-07-22 23:17:08 +0200

    Automatically autocomplete on click.

--- src/autocomplete.php
+++ src/autocomplete.php
@@ -34,6 +34,7 @@ function autocomplete(inp, arr) {
               /*close the list of autocompleted values,
               (or any other open lists of autocompleted values:*/
               closeAllLists();
+              document.getElementsByName("open")[0].click();
           });
           a.appendChild(b);
         }
