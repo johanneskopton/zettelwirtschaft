c27af5d77e022e2a918ef994e2f234a8e3b8b744
Johannes Kopton <johanneskopton@gmail.com>
2021-08-13 22:20:21 +0200

    Throw error on connection file while creating db.

--- create_user_db.php
+++ create_user_db.php
@@ -4,6 +4,11 @@
 
     $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
+    // Check connection
+    if ($mysqli->connect_error) {
+      die("Connection failed: " . $mysqli->connect_error);
+    }
+
     # STRICTLY RESTRICT ACCESS TO EXECUTE THIS FILE
     # OR COMMENT OUT THESE LINES AFTER DB CREATION
     $sql = file_get_contents("database/user.sql");
